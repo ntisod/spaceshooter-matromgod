77b70e84923ecf5fa2a7793d53a52ce29c545a5f
matromgod <matheofabian.romerogamero@elev.ga.ntig.se>
2019-10-10 13:29:58 +0200

    Update Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -11,12 +11,8 @@ namespace SpaceShooter
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        
 
-        //Variabler
-        Texture2D ship_texture;
-        Vector2 ship_vector; //Rymdsheppets position
-        Vector2 ship_speed; //Rymdsheppets hastighet
+        Player player;
 
         public Game1()
         {
@@ -24,22 +20,8 @@ namespace SpaceShooter
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            ship_vector.X = 380;
-            ship_vector.Y = 400;
-
-            ship_speed.X = 50f;
-            ship_speed.Y = 50f;
-
-
             base.Initialize();
         }
 
@@ -49,11 +31,12 @@ namespace SpaceShooter
         /// </summary>
         protected override void LoadContent()
         {
-            ship_texture = Content.Load<Texture2D>("ship");
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            player = new Player(Content.Load<Texture2D>("ship"), 380, 400,2.5f, 4.5f);
+
         }
 
         /// <summary>
@@ -72,41 +55,7 @@ namespace SpaceShooter
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-
-            KeyboardState keyboardState = Keyboard.GetState();
-
-            if (ship_vector.X <= Window.ClientBounds.Width - ship_texture.Width && ship_vector.X >= 0)
-            {
-                if (keyboardState.IsKeyDown(Keys.Right))
-                    ship_vector.X += ship_speed.X;
-                if (keyboardState.IsKeyDown(Keys.Left))
-                    ship_vector.X -= ship_speed.X;
-            }
-
-            if (ship_vector.Y <= Window.ClientBounds.Height - ship_texture.Height && ship_vector.Y >= 0)
-            {
-                if (keyboardState.IsKeyDown(Keys.Down))
-                    ship_vector.Y += ship_speed.Y;
-                if (keyboardState.IsKeyDown(Keys.Up))
-                    ship_vector.Y -= ship_speed.Y;
-            }
-
-            if (ship_vector.X < 0)
-                ship_vector.X = 0;
-            if (ship_vector.X > Window.ClientBounds.Width - ship_texture.Width)
-            {
-                ship_vector.X = Window.ClientBounds.Width - ship_texture.Width;
-            }
-
-            if (ship_vector.Y < 0)
-                ship_vector.Y = 0;
-            if (ship_vector.Y > Window.ClientBounds.Height - ship_texture.Height)
-            {
-                ship_vector.Y = Window.ClientBounds.Height - ship_texture.Height;
-            }
+            player.Update(Window);
 
             base.Update(gameTime);
         }
@@ -122,7 +71,7 @@ namespace SpaceShooter
 
             // TODO: Add your drawing code here'
             spriteBatch.Begin();
-            spriteBatch.Draw(ship_texture, ship_vector, Color.White);
+            player.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
